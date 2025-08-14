81cae7d900364cd0580db88556a87e599c6b35d7
qkhai15 <2251061804@e.tlu.edu.vn>
2025-08-14 22:18:12 +0700

    Giao dien

--- app/src/main/java/com/example/hungthinhapartmentmanagement/MainActivity.java
+++ app/src/main/java/com/example/hungthinhapartmentmanagement/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_request_list);
+        setContentView(R.layout.activity_invoice);
     }
 }
